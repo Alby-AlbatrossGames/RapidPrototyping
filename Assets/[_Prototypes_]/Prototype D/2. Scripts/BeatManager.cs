4a1c4c3b2d45ca98f25c79c7b18ef3ed49e72454
Albytrosss <AlbatrossGames@pm.me>
2025-07-24 22:33:15 +1000

    Adjusted Timing + Added Directional Feedback

--- Assets/[_Prototypes_]/Prototype D/2. Scripts/BeatManager.cs
+++ Assets/[_Prototypes_]/Prototype D/2. Scripts/BeatManager.cs
@@ -41,28 +41,30 @@ namespace Prototype4
             Debug.Log("Beat Start");
             EventManager.ReportOnBeatStart();
 
+            testObj.GetComponent<Renderer>().material.color = Color.blue;
+
             sliderVal = RightSlider.minValue;
 
             ExecuteAfterSeconds((beatDuration/8) *1, EndPerfectBeatWindow);
-            ExecuteAfterSeconds((beatDuration/8) *3, EndGoodBeatWindow);
-            ExecuteAfterSeconds((beatDuration/8) *6, EndOKBeatWindow);
+            ExecuteAfterSeconds((beatDuration/8) *2, EndGoodBeatWindow);
+            ExecuteAfterSeconds((beatDuration/8) *4, EndOKBeatWindow);
             // ExecuteAfterSeconds((beatDuration / 8) * 7, StartPerfectBeatWindow);
             ExecuteAfterSeconds(beatDuration, StartBeat);
         }
         private void EndPerfectBeatWindow()
         {
             EventManager.ReportOnPerfectWindowEnd();
-            testObj.GetComponent<Renderer>().material.color = Color.green;
+            testObj.GetComponent<Renderer>().material.color = Color.yellow;
         }
         private void EndGoodBeatWindow()
         {
             EventManager.ReportOnGoodWindowEnd();
-            testObj.GetComponent<Renderer>().material.color = Color.yellow;
+            testObj.GetComponent<Renderer>().material.color = Color.red;
         }
         private void EndOKBeatWindow()
         {
             EventManager.ReportOnOKWindowEnd();
-            testObj.GetComponent<Renderer>().material.color = Color.red;
+            //testObj.GetComponent<Renderer>().material.color = Color.red;
         }
         /*private void StartPerfectBeatWindow()
         {
--- Assets/[_Prototypes_]/Prototype D/2. Scripts/InputManager.cs
+++ Assets/[_Prototypes_]/Prototype D/2. Scripts/InputManager.cs
@@ -10,7 +10,10 @@ namespace Prototype4
         public enum BeatTiming { MISS, PERFECT, GOOD, OK }
         public BeatTiming beatTiming = BeatTiming.MISS;
 
-        public TMP_Text testText;
+        public TMP_Text upFeedback;
+        public TMP_Text downFeedback;
+        public TMP_Text leftFeedback;
+        public TMP_Text rightFeedback;
 
         void SetTimingPERFECT() => beatTiming = BeatTiming.PERFECT;
         void SetTimingGOOD() => beatTiming = BeatTiming.GOOD;
@@ -31,40 +34,104 @@ namespace Prototype4
             switch (beatTiming)
             {
                 case BeatTiming.MISS:
-                    testText.text = "Miss...";
-                    testText.color = Color.red;
+                    upFeedback.text = "Miss...";
+                    upFeedback.color = Color.red;
                     break;
                 case BeatTiming.OK:
-                    testText.text = "OK!";
-                    testText.color = Color.yellow;
+                    upFeedback.text = "OK";
+                    upFeedback.color = Color.yellow;
                     break;
                 case BeatTiming.GOOD:
-                    testText.text = "Good!";
-                    testText.color = Color.green;
+                    upFeedback.text = "Good!";
+                    upFeedback.color = Color.green;
                     break;
                 case BeatTiming.PERFECT:
-                    testText.text = "Perfect!";
-                    testText.color = Color.blue;
+                    upFeedback.text = "Perfect!";
+                    upFeedback.color = Color.blue;
                     break;
                 default:
-                    testText.text = "ERROR";
-                    testText.color = Color.black;
+                    upFeedback.text = "ERROR";
+                    upFeedback.color = Color.black;
                     break;
-
-
             }
         }
         void OnDownAction()
         {
-            Debug.Log("DOWN");
+            switch (beatTiming)
+            {
+                case BeatTiming.MISS:
+                    downFeedback.text = "Miss...";
+                    downFeedback.color = Color.red;
+                    break;
+                case BeatTiming.OK:
+                    downFeedback.text = "OK!";
+                    downFeedback.color = Color.yellow;
+                    break;
+                case BeatTiming.GOOD:
+                    downFeedback.text = "Good!";
+                    downFeedback.color = Color.green;
+                    break;
+                case BeatTiming.PERFECT:
+                    downFeedback.text = "Perfect!";
+                    downFeedback.color = Color.blue;
+                    break;
+                default:
+                    downFeedback.text = "ERROR";
+                    downFeedback.color = Color.black;
+                    break;
+            }
         }
         void OnLeftAction()
         {
-            Debug.Log("LEFT");
+            switch (beatTiming)
+            {
+                case BeatTiming.MISS:
+                    leftFeedback.text = "Miss...";
+                    leftFeedback.color = Color.red;
+                    break;
+                case BeatTiming.OK:
+                    leftFeedback.text = "OK!";
+                    leftFeedback.color = Color.yellow;
+                    break;
+                case BeatTiming.GOOD:
+                    leftFeedback.text = "Good!";
+                    leftFeedback.color = Color.green;
+                    break;
+                case BeatTiming.PERFECT:
+                    leftFeedback.text = "Perfect!";
+                    leftFeedback.color = Color.blue;
+                    break;
+                default:
+                    leftFeedback.text = "ERROR";
+                    leftFeedback.color = Color.black;
+                    break;
+            }
         }
         void OnRightAction()
         {
-            Debug.Log("RIGHT");
+            switch (beatTiming)
+            {
+                case BeatTiming.MISS:
+                    rightFeedback.text = "Miss...";
+                    rightFeedback.color = Color.red;
+                    break;
+                case BeatTiming.OK:
+                    rightFeedback.text = "OK!";
+                    rightFeedback.color = Color.yellow;
+                    break;
+                case BeatTiming.GOOD:
+                    rightFeedback.text = "Good!";
+                    rightFeedback.color = Color.green;
+                    break;
+                case BeatTiming.PERFECT:
+                    rightFeedback.text = "Perfect!";
+                    rightFeedback.color = Color.blue;
+                    break;
+                default:
+                    rightFeedback.text = "ERROR";
+                    rightFeedback.color = Color.black;
+                    break;
+            }
         }
         #endregion Input Actions
     }
